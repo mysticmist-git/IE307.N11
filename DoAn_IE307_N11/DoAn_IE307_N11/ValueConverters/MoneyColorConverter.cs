00cb86361cc9fc5287d3bedf6a2c96eb0f04221e
Nguyen Van Hen <89691660+String1502@users.noreply.github.com>
2022-11-02 12:28:47 +0700

    add-converters

---
+++ DoAn_IE307_N11/DoAn_IE307_N11/ValueConverters/MoneyColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.CommunityToolkit.Converters;
+using Xamarin.Forms;
+
+namespace DoAn_IE307_N11.ValueConverters
+{
+    public class MoneyColorConverter : BaseValueConverter<MoneyColorConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var money = (int)value;
+
+            if (money < 0)
+                return Color.Red;
+
+            return Color.Green;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
